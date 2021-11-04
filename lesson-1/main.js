3b8771f97676f9a6426bb8aa0d2f0f4acd7cc7c8
Artemii-Belousov <neochoto@mail.ru>
2021-11-04 19:06:23 +0500

    lesson-1

--- lesson-1/main.js
+++ lesson-1/main.js
@@ -1,30 +1,44 @@
-const products = [
-    {id: 1, title: 'Samsung', price: 40000, img: 'https://picsum.photos/600/300'},
-    {id: 1, title: 'Apple', price: 65000, img: 'https://picsum.photos/600/300'},
-    {id: 1, title: 'LG', price: 35000, img: 'https://picsum.photos/600/300'},
-    {id: 1, title: 'Honor', price: 50000, img: 'https://picsum.photos/600/300'},
+const products = [{
+        id: 1,
+        title: 'Samsung',
+        price: 40000,
+        img: 'https://picsum.photos/600/300'
+    },
+    {
+        id: 2,
+        title: 'Apple',
+        price: 65000,
+        img: 'https://picsum.photos/600/300'
+    },
+    {
+        id: 3,
+        title: 'LG',
+        price: 35000,
+        img: 'https://picsum.photos/600/300'
+    },
+    {
+        id: 4,
+        title: 'Honor',
+        price: 50000,
+        img: 'https://picsum.photos/600/300'
+    },
 ];
 
-const renderProduct = (title, price, img) => 
+const renderProduct = (x) =>
     `<div class="product-item">
-        <img srs = ${img}>
-        <h3>${title}</h3>
-        <p>${price}</p>
-        <button class="buy-btn">Купить</button>
+        <img src = ${x.img}>
+        <div class="product-item__discription">
+            <h3>${x.title}</h3>
+            <p>${x.price}</p>
+            <button class="buy-btn">Купить</button>
+        </div>
     </div>`;
 
 const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item.title, item.price, item.img));
-    console.log(productsList);
-    document.querySelector('.products').innerHTML = productsList
+    //массив преобразуется в строку (и использует запятую в качестве разделителя по умолчанию)
+    const productsList = list.map(item => renderProduct(item));
+    //Метод join() объединяет все элементы массива (или массивоподобного объекта) в строку. В качестве разделителя задается пустая строка.
+    document.querySelector('.products').innerHTML = productsList.join("")
 };
 
-
-
 renderPage(products);
-
-
-
-
-
-

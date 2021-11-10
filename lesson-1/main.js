28c307fc72c7431ea3ddad715e1c84e64a0ad5a0
Artemii-Belousov <neochoto@mail.ru>
2021-11-10 21:00:04 +0500

    lesson-2

--- lesson-1/main.js
+++ lesson-1/main.js
@@ -1,27 +1,14 @@
-const products = [{
-        id: 1,
-        title: 'Samsung',
-        price: 40000,
-        img: 'https://picsum.photos/600/300'
-    },
-    {
-        id: 2,
-        title: 'Apple',
-        price: 65000,
-        img: 'https://picsum.photos/600/300'
-    },
-    {
-        id: 3,
-        title: 'LG',
-        price: 35000,
-        img: 'https://picsum.photos/600/300'
-    },
-    {
-        id: 4,
-        title: 'Honor',
-        price: 50000,
-        img: 'https://picsum.photos/600/300'
-    },
+const products = [
+    {   id: 1, title: 'Samsung', price: 400,img: 'https://picsum.photos/600/300'},
+    {   id: 2, title: 'Apple', price: 65000,img: 'https://picsum.photos/600/300'},
+    {   id: 3, title: 'LG', price: 35000, img: 'https://picsum.photos/600/300'},    
+    {   id: 4, title: 'Honor', price: 50000,img: 'https://picsum.photos/600/300'},
+];
+const products2 = [
+    {   id: 1, title: 'HTC', price: 400,img: 'https://picsum.photos/600/300'},
+    {   id: 2, title: 'Philips', price: 65000,img: 'https://picsum.photos/600/300'},
+    {   id: 3, title: 'Sony', price: 35000, img: 'https://picsum.photos/600/300'},    
+    {   id: 4, title: 'Lenovo', price: 50000,img: 'https://picsum.photos/600/300'},
 ];
 
 const renderProduct = (x) =>
@@ -35,10 +22,17 @@ const renderProduct = (x) =>
     </div>`;
 
 const renderPage = list => {
+
+    document.querySelector('.products').innerHTML = list.map(product => renderProduct(product)).join(''); 
+
     //массив преобразуется в строку (и использует запятую в качестве разделителя по умолчанию)
-    const productsList = list.map(item => renderProduct(item));
+    // const productsList = list.map(item => renderProduct(item));
     //Метод join() объединяет все элементы массива (или массивоподобного объекта) в строку. В качестве разделителя задается пустая строка.
-    document.querySelector('.products').innerHTML = productsList.join("")
+    //document.querySelector('.products').innerHTML = productsList.join("")
+    
 };
 
-renderPage(products);
+
+
+renderPage(products.concat(products2));
+
